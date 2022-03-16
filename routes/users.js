62b78455e8e507df095425399b82e3c244c797ae
Giribabu Velamunaidu <giribabuvelamunaidu@AGLS-MLT-483.local>
2022-03-16 12:36:11 +0530

    server

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,14 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { genPassword } from "../genPassword.js";
 import { clinet } from "../index.js";
+import { get_all_userid } from "../helper/get_all_userid.js";
 const router = express.Router();
-
+router.get("/", async function (request, response) {
+  const users = await get_all_userid();
+  response.send(users);
+});
 router.post("/signup", async function (request, response) {
-  const { user, password } = request.body;
+  const { username, password } = request.body;
   const hashpassword = await genPassword(password);
   const newUser = {
     username: username,
@@ -18,7 +22,7 @@ router.post("/signup", async function (request, response) {
 router.post("/login", async function (request, response) {
   const { username, password } = request.body;
 
-  const userFromDB = await client
+  const userFromDB = await clinet
     .db("ag")
     .collection("users")
     .findOne({ username: username });
@@ -32,7 +36,7 @@ router.post("/login", async function (request, response) {
   const isPasswordMatch = await bcrypt.compare(password, storePassword);
 
   if (isPasswordMatch) {
-    const token = jwt.sign({ id: userFromDB._id }, process.env.SECRET_KEY);
+    const token = jwt.sign({ id: userFromDB._id }, process.env.token_key);
     response.send({ message: "Successfull login", token: token });
   } else {
     response.status(401).send({ message: "Invalid credentials" });
